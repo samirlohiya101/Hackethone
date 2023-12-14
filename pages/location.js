ab4adc03b886ffb797f099995e3c3735638b2e28
samirlohiya101 <samir.lohiya@intoglo.com>
2023-12-14 16:38:27 +0530

    added location api

---
+++ pages/location.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapboxAutoComplete from '../components/MapboxAutoComplete';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Mapbox Autocomplete Example</h1>
+            <MapboxAutoComplete  />
+        </div>
+    );
+};
+
+export default Home;
